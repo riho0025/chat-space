7fd6d9dfb5acc13242ad9fcc31a9bca2162854cd
riho0025 <yuxxx26xx@gmail.com>
2020-04-26 20:18:21 +0900

    rewrited!

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -66,18 +66,18 @@ $(function () {
       data: { id: last_message_id }
     })
       .done(function (messages) {
-        if (messages.length !== 0) {
-          var insertHTML = '';
-          $.each(messages, function (i, message) {
-            insertHTML += buildHTML(message)
-          });
-          $('.messages').append(insertHTML);
-          $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
-        }
-      })
-      .fail(function () {
-        alert('error');
-      });
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function (i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
+      }
+    })
+    .fail(function () {
+      alert('error');
+    });
   };
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
     setInterval(reloadMessages, 7000);
